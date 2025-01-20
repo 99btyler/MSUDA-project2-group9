5e65895afd9035b3d01f912a314c3bb744368b5f
Tyler Beringer <99tylerberinger@gmail.com>
2025-01-20 11:07:44 -0500

    Added description to queries, and 2 new queries by vhrico from Slack

--- submission/part3.1_queries.sql
+++ submission/part3.1_queries.sql
@@ -1,17 +1,31 @@
--- CAMPAIGNS (successful): company_name, outcome, goal, pledged, backers_count, launch_date, end_date
-SELECT company_name, outcome, goal, pledged, backers_count, launch_date, end_date
+-- CAMPAIGNS (successful): company_name, description, outcome, goal, pledged, backers_count, launch_date, end_date
+SELECT company_name, description, outcome, goal, pledged, backers_count, launch_date, end_date
 FROM campaign
 WHERE outcome = 'successful'
 ORDER BY company_name;
 
--- CAMPAIGNS (failed): company_name, outcome, goal, pledged, backers_count, launch_date, end_date
-SELECT company_name, outcome, goal, pledged, backers_count, launch_date, end_date
+-- CAMPAIGNS (number of successful campaigns by country): country, successful_campaigns
+SELECT country, COUNT(*) AS successful_campaigns
+FROM campaign
+WHERE outcome = 'successful'
+GROUP BY country
+ORDER BY successful_campaigns DESC;
+
+-- CAMPAIGNS (failed): company_name, description, outcome, goal, pledged, backers_count, launch_date, end_date
+SELECT company_name, description, outcome, goal, pledged, backers_count, launch_date, end_date
 FROM campaign
 WHERE outcome = 'failed'
 ORDER BY company_name;
 
--- CAMPAIGNS (highest pledged): company_name, goal, pledged, outcome
-SELECT company_name, goal, pledged, outcome
+-- CAMPAIGNS (number of failed campaigns by country): country, failed_campaigns
+SELECT country, COUNT(*) AS failed_campaigns
+FROM campaign
+WHERE outcome = 'failed'
+GROUP BY country
+ORDER BY failed_campaigns DESC;
+
+-- CAMPAIGNS (highest pledged): company_name, description, goal, pledged, outcome
+SELECT company_name, description, goal, pledged, outcome
 FROM campaign
 ORDER BY pledged DESC;
 
